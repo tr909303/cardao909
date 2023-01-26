c81920c0e4bce90ad0b448ce6b06736416b92771
tr909303 <74880698+tr909303@users.noreply.github.com>
2023-01-26 15:37:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// Select the navigation links
+// Get all the navigation links
 const navLinks = document.querySelectorAll("nav a");
 
-// Add an event listener to each link
+// Add event listeners to the navigation links
 navLinks.forEach(link => {
   link.addEventListener("click", e => {
     e.preventDefault();
     // Get the id of the section to show
-    const sectionId = e.target.getAttribute("href");
-    // Get the section
-    const section = document.querySelector(sectionId);
+    const sectionId = link.getAttribute("href");
     // Show the section
-    section.style.display = "block";
+    document.querySelector(sectionId).style.display = "block";
     // Hide all other sections
-    const sections = document.querySelectorAll("main > *:not(#" + sectionId.slice(1) + ")");
+    const sections = document.querySelectorAll("main > *:not(" + sectionId + ")");
     sections.forEach(s => {
       s.style.display = "none";
     });
